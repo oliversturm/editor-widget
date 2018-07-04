61755c4bd327ccb9aaa7d66d3ebded1e5659892b
Oliver Sturm <oliver@oliversturm.com>
2018-07-04 17:08:16 +0100

    updated package refs, small fixes

--- bin/editor-widget.js
+++ bin/editor-widget.js
@@ -4,6 +4,13 @@ var blessed = require('blessed');
 var Editor = require('../.');
 
 var screen = new blessed.Screen();
-screen.key('C-q', function () { process.exit(); });
-var editor = new Editor({parent: screen});
+screen.key('C-q', function() {
+  process.exit();
+});
+var editor = new Editor({
+  parent: screen,
+  text: 'test text'
+  //, highlight: true
+});
 editor.open(__filename).done();
+//editor.language('js');
